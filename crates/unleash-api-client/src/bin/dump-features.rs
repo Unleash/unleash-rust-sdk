2ce5831f930f89448ba58498d75ebecefcd30395
Simon Hornby <liquidwicked64@gmail.com>
2026-07-15 15:36:59 +0200

    chore: update dump features

--- crates/unleash-api-client/src/bin/dump-features.rs
+++ crates/unleash-api-client/src/bin/dump-features.rs
@@ -2,14 +2,14 @@
 use async_std::task;
 use uuid::Uuid;
 
-use unleash_api_client::api;
 use unleash_api_client::config::EnvironmentConfig;
 use unleash_api_client::http;
+use unleash_yggdrasil::UpdateMessage;
 
 fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
     task::block_on(async {
         let config = EnvironmentConfig::from_env()?;
-        let endpoint = api::Features::endpoint(&config.api_url);
+        let endpoint = format!("{}/client/features", config.api_url.trim_end_matches('/'));
         let client = http::Http::new(
             http::default_transport(),
             config.app_name,
@@ -17,7 +17,7 @@ fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
             Uuid::new_v4().to_string(),
             config.secret,
         );
-        let res: api::Features = client.get_json(&endpoint, None).await?;
+        let res: UpdateMessage = client.get_json(&endpoint, None).await?;
         dbg!(res);
         Ok(())
     })
